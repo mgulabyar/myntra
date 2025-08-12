d5f20e300d60626b98ee434bb0d116552e35adf6
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2025-08-12 10:35:38 +0500

    fix: patch async payment gateway configuration in donation context (part 3)

--- src/components/dynamic/CampaignItem.tsx
+++ src/components/dynamic/CampaignItem.tsx
@@ -1,2 +1,3 @@
 // Finalizing theme configuration structures for checkout donation form // Update verification token: 0d198d96
 // TODO: Refactor payment and donor service processing wrapper // Update verification token: a9319b93
+// TODO: Refactor payment and donor service processing wrapper // Update verification token: b4d8ecac
