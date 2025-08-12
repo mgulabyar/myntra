90a1b65494e80353f19e484c83cab6a564fbcddc
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2025-08-12 14:48:27 +0500

    feat: inject custom typography components for target progress bar metrics (part 2)

--- src/components/dynamic/CampaignItem.tsx
+++ src/components/dynamic/CampaignItem.tsx
@@ -1,1 +1,2 @@
 // Finalizing theme configuration structures for checkout donation form // Update verification token: 0d198d96
+// TODO: Refactor payment and donor service processing wrapper // Update verification token: a9319b93
