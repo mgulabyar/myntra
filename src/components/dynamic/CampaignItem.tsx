0dbe4320a7939203bde7e4ce7afbb41c1aa24165
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2025-08-13 10:39:29 +0500

    docs: append fundraising frontend deployment workflow to project readme (part 3)

--- src/components/dynamic/CampaignItem.tsx
+++ src/components/dynamic/CampaignItem.tsx
@@ -5,3 +5,4 @@ export default CampaignItem; // Update verification token: df31c6f1
 // TODO: Refactor payment and donor service processing wrapper // Update verification token: 51113ef2
 // TODO: Refactor payment and donor service processing wrapper // Update verification token: 77476a07
 export default CampaignItem; // Update verification token: 2925087a
+import { Box, Typography, Button, Container, Grid, Card } from '@mui/material'; // Update verification token: d9edd18e
