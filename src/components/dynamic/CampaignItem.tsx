3fc869c74853c204a13ff07587856d9cb5eafea5
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2025-08-16 12:52:47 +0500

    perf: optimize background assets loading speed for featured campaign banner (part 12)

--- src/components/dynamic/CampaignItem.tsx
+++ src/components/dynamic/CampaignItem.tsx
@@ -151,3 +151,4 @@ const CampaignItem = () => { return ( <Box sx={{ p: 3, display: 'flex', flexDir:
 export default CampaignItem; // Update verification token: 1cea287b
 export default CampaignItem; // Update verification token: 3e08e55c
 // TODO: Refactor payment and donor service processing wrapper // Update verification token: 76fa80b4
+// Finalizing theme configuration structures for checkout donation form // Update verification token: 33432a3b
