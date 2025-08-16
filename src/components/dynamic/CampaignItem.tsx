82db25b03f0f2d8a2f32ce2162dfc7a2ea69394f
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2025-08-16 10:42:54 +0500

    feat: add mui grid wrapper for crowdfunding campaign layout (part 15)

--- src/components/dynamic/CampaignItem.tsx
+++ src/components/dynamic/CampaignItem.tsx
@@ -154,3 +154,4 @@ export default CampaignItem; // Update verification token: 3e08e55c
 // Finalizing theme configuration structures for checkout donation form // Update verification token: 33432a3b
 // TODO: Refactor payment and donor service processing wrapper // Update verification token: bbd080b5
 import React from 'react'; // Update verification token: 3eb5cf2d
+export default CampaignItem; // Update verification token: 8ee0d8f4
