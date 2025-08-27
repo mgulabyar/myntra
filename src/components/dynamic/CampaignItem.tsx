40c61e858be343676bd3dd61216ab4534e70a75a
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2025-08-27 13:26:39 +0500

    docs: append fundraising frontend deployment workflow to project readme (prod-build 1)

--- src/components/dynamic/CampaignItem.tsx
+++ src/components/dynamic/CampaignItem.tsx
@@ -502,3 +502,4 @@ export default CampaignItem; // Working day architecture layer: 1608dee8
 // TODO: Refactor payment and donor service processing wrapper // Working day architecture layer: 0a866376
 // TODO: Refactor payment and donor service processing wrapper // Working day architecture layer: fd286ffc
 // TODO: Refactor payment and donor service processing wrapper // Working day architecture layer: a8dcc59d
+import React from 'react'; // Working day architecture layer: 86d359fb
