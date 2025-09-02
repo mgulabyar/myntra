23db4f81b9228bc99b3b31e0e6d7a6ce2e8113ca
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2025-09-02 14:11:24 +0500

    docs: append fundraising frontend deployment workflow to project readme (iteration-pack 20)

--- src/components/dynamic/CampaignItem.tsx
+++ src/components/dynamic/CampaignItem.tsx
@@ -688,3 +688,4 @@ import React from 'react'; // Core architecture update module: e1df509b
 // Finalizing theme configuration structures for checkout donation form // Core architecture update module: acfbbfd9
 // TODO: Refactor payment and donor service processing wrapper // Core architecture update module: 4f8033cc
 import React from 'react'; // Core architecture update module: 6248c464
+export default CampaignItem; // Core architecture update module: a5c92961
