61209fb4d5f94e312b69b10432cf06785e519381
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-07-06 16:26:20 +0500

    feat: build dynamic individual campaign detail page layout

--- src/components/extrea.tsx
+++ src/components/extrea.tsx
@@ -53,7 +53,6 @@ const Navbar: React.FC = () => {
     </button>
   )}
 
-  {/* Modal */}
   {showModal && (
     <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg w-96">
