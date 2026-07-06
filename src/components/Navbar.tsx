d50e1bcffc6e48b822d4ed7013ebd72a64dd2aee
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-07-06 16:31:52 +0500

    fix: resolve mobile navigation overlay toggle bugs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -160,7 +160,6 @@ const Navbar: React.FC = () => {
           </div>
         )}
 
-        {/*  Login Modal */}
         {showModal && (
           <div className="fixed inset-0 flex items-center justify-center bg-black/50 z-50">
             <div className="bg-white p-6 rounded-lg shadow-lg w-96">
