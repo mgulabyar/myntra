fc06ff18c09bb0b125029300bef85e2b2c320f39
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-07-07 19:32:54 +0500

    feat: add staggered fade-up entrance animation for hero content

--- src/components/SectionOne.tsx
+++ src/components/SectionOne.tsx
@@ -38,7 +38,7 @@
 
 
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const SectionOne = () => {
   const [mounted, setMounted] = useState(false);
