909196f1e2b8a421ea84dcc6a2bad3552e9212c3
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-07-07 23:53:30 +0500

    style: update navbar link colors and typography to match branding

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar: React.FC = () => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
-//  use effect 
+//  use  
   useEffect(() => {
     document.body.style.overflow = showModal || isOpen ? "hidden" : "";
     return () => {
