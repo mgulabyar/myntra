532b1fbbf487eec80da68cb87d750c3a922ed3d2
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-07-08 12:54:57 +0500

    feat: add loading state to Show More button

--- src/cards/Card1.tsx
+++ src/cards/Card1.tsx
@@ -229,7 +229,6 @@ const CampaignCards = () => {
       setLoadingMore(false);
     }, 300);
   };
-//  setvi
   const allShown = visibleCount >= campaigns.length;
   return (
     <div className="flex flex-col items-center px-4 py-8 bg-gray-50">
