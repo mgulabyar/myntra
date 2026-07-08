4bb12333bbd383a592a06dfe63121171efdd0136
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-07-08 13:04:48 +0500

    style: add card lift and shadow transition on hover

--- src/footer/Start.tsx
+++ src/footer/Start.tsx
@@ -4,8 +4,6 @@
 //   return (
 //     <div className='bg-[#f5f5f5] p-[48px] flex items-center gap-10 justify-center'>
 // <h1 className="text-3xl md:4xl lg:5xl font-semibold text-[#262626]">Fund your fight for social justice with Chuffed</h1>
-// <button className='hover:outline-2 hover:outline-offset-2 hover:outline-pink-800 cursor-pointer  bg-[#e444a0] hover:bg-pink-700 text-white rounded-md px-4 py-2'>
-// <a href="https://chuffed.org/onboarding/category">Start Your Campaign </a>
 
 
 
